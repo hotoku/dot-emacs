d463517347e3426ba9212112042d29c47691fb75
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2021-07-24 09:20:28 +0000

    gnu-elpa-keyring-update

--- init.el
+++ init.el
@@ -55,7 +55,11 @@ The last executing date is recorded in the FILENAME in `user-emacs-directory.'"
 
 
 ;;; configuration of packagseo
-(use-package gnu-elpa-keyring-update)
+(use-package gnu-elpa-keyring-update
+  :config
+  (unless (file-directory-p package-gnupghome-dir)
+    (make-directory package-gnupghome-dir))
+  (gnu-elpa-keyring-update))
 
 (use-package exec-path-from-shell
   :config
@@ -66,30 +70,30 @@ The last executing date is recorded in the FILENAME in `user-emacs-directory.'"
 (use-package yh-blog :ensure nil)
 
 (use-package yh-fef :ensure nil
-             :hook
-             (emacs-lisp-mode . (lambda () (add-hook 'before-save-hook 'yh-fef-format-buffer nil t))))
+  :hook
+  (emacs-lisp-mode . (lambda () (add-hook 'before-save-hook 'yh-fef-format-buffer nil t))))
 
 (use-package yh-docker :ensure nil
-             :hook
-             (dockerfile-mode
-              .
-              (lambda ()
-                (add-hook 'before-save-hook 'yh-docker-upcase-command nil t))))
+  :hook
+  (dockerfile-mode
+   .
+   (lambda ()
+     (add-hook 'before-save-hook 'yh-docker-upcase-command nil t))))
 
 (use-package yh-font :ensure nil
-             :config
-             (yh-font-initialize))
+  :config
+  (yh-font-initialize))
 
 (use-package yh-make :ensure nil)
 
 (use-package yh-sh :ensure nil)
 
 (use-package yh-sql :ensure nil
-             :hook
-             (sql-mode
-              .
-              (lambda ()
-                (add-hook 'after-save-hook 'yh-sql-format nil t))))
+  :hook
+  (sql-mode
+   .
+   (lambda ()
+     (add-hook 'after-save-hook 'yh-sql-format nil t))))
 
 (use-package lsp-mode
   :init
@@ -534,7 +538,7 @@ This is inconvinient when opening file at the beginning of Emacs session."
  '(custom-safe-themes
    '("57e3f215bef8784157991c4957965aa31bac935aca011b29d7d8e113a652b693" "246cd0eb818bfd347b20fb6365c228fddf24ab7164752afe5e6878cb29b0204e" default))
  '(package-selected-packages
-   '(lsp-mode hcl-mode direx dired-k exec-path-from-shell dired yh-make il lsp-docker poetry gitignore-mode helm-ag pyenv afuternoon-theme afternoon-theme bazel-mode pyenv-mode-auto prettier-js dap-python py-autopep8 flymake-yaml dockerfile-mode biblio elpy haskell-mode yaml-mode json-mode gnu-elpa-keyring-update undo-tree git-ps1-mode ace-window flycheck yasnippet open-junk-file dakrone-theme smartparens helm company use-package)))
+   '(spinner gnupg lsp-mode hcl-mode direx dired-k exec-path-from-shell dired yh-make il lsp-docker poetry gitignore-mode helm-ag pyenv afuternoon-theme afternoon-theme bazel-mode pyenv-mode-auto prettier-js dap-python py-autopep8 flymake-yaml dockerfile-mode biblio elpy haskell-mode yaml-mode json-mode gnu-elpa-keyring-update undo-tree git-ps1-mode ace-window flycheck yasnippet open-junk-file dakrone-theme smartparens helm company use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

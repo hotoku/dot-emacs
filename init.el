d74573e8d377461f2762a9335edd6e9cf5b4d21d
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2021-07-24 09:41:56 +0900

    refresh

--- init.el
+++ init.el
@@ -8,6 +8,23 @@
 
 
 ;;; package
+(defun yh-doit-if-not-yet-today (func filename)
+  "Call FUNC if it is not executed today.
+The last executing date is recorded in the FILENAME in `user-emacs-directory.'"
+  (let* ((path (expand-file-name filename user-emacs-directory))
+         (today (format-time-string "%Y-%m-%d"))
+         (last-date (when (file-exists-p path)
+                      (with-temp-buffer
+                        (insert-file-contents path)
+                        (buffer-substring 1 11))))
+         (should-update (or (not last-date)
+                            (string< last-date today))))
+    (when should-update
+      (funcall func)
+      (with-temp-buffer
+        (insert today)
+        (write-region (point-min) (point-max) path)))))
+
 (require 'package)
 (setq package-archives '(("org" . "http://orgmode.org/elpa/")
                          ("gnu" . "http://elpa.gnu.org/packages/")
@@ -15,20 +32,9 @@
 
 
 ;;; package-refresh-contents
-;; Package-refresh-contents if last update is older than or equal to yesterday.
-(let* ((path (expand-file-name ".date-of-last-package-refresh-contents" user-emacs-directory))
-       (today (format-time-string "%Y-%m-%d"))
-       (last-date (when (file-exists-p path)
-                    (with-temp-buffer
-                      (insert-file-contents path)
-                      (buffer-substring 1 11))))
-       (should-update (or (not last-date)
-                          (string< last-date today))))
-  (when should-update
-    (package-refresh-contents)
-    (with-temp-buffer
-      (insert today)
-      (write-region (point-min) (point-max) path))))
+(yh-doit-if-not-yet-today 'package-refresh-contents
+                          ".date-of-last-package-refresh-contents")
+
 (package-initialize)
 
 

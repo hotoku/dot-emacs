2199191b2e3c45b8a3532f7893caf7064d0cdf28
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2021-08-15 13:28:58 +0900

    this does not work

--- yh/yh-save.el
+++ yh/yh-save.el
@@ -22,5 +22,18 @@ CONFIG should be a list of the following symbols.
                   (add-hook 'before-save-hook func nil t)))
             config)))
 
+(defmacro yh-before-save2 (&rest config)
+  (let ((key2func '((:indent . yh/indent-buffer)
+                    (:space . delete-trailing-whitespace)
+                    (:gap . yh-space-make-gap-buffer))))
+    `(lambda ()
+       ,@(mapcar (lambda (key) `(add-hook 'before-save-hook (quote ,(cdr (assoc key key2func))) nil t)) config))))
+
+(yh-before-save2 :config :space)
+
+(lambda ()
+  (add-hook 'before-save-hook 'yh/indent-buffer nil t)
+  (add-hook 'before-save-hook 'delete-trailing-whitespace nil t))
+
 (provide 'yh-save)
 ;;; yh-save.el ends here

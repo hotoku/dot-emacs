1052e9175e5d852298f02e2957c3bad8cbd3911f
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2021-12-13 18:09:22 +0900

    [yh-sql] when flag is nil, do nothing

--- site-lisp/yh/yh-sql.el
+++ site-lisp/yh/yh-sql.el
@@ -25,29 +25,30 @@
   "Format buffer with zetasql-formatter.
 https://github.com/Matts966/zetasql-formatter."
   (interactive)
-  (let* ((curbuf (current-buffer))
-         (curpnt (point))
-         (outbuf-name "*zetasql*")
-         (outbuf (get-buffer outbuf-name))
-         (fpath (buffer-file-name))
-         (command (yh-sql-format-command fpath)))
-    (when outbuf
-      (save-excursion
-        (switch-to-buffer outbuf)
-        (erase-buffer)))
-    ;; DIRTY HACK.
-    ;; zetasql-formatter return non-zero value when the formatted string is
-    ;; different from the original even if it was syntactically correct.
-    ;; Here, we want to change to *zetasql* buffern only when it is syntactically
-    ;; incorrect. We format the file twice and check the output of second run.
-    (let ((ret (progn (call-process-shell-command command
-                                                  nil outbuf-name)
-                      (call-process-shell-command command
-                                                  nil outbuf-name))))
-      (if (= ret 0)
-          (progn (switch-to-buffer curbuf)
-                 (revert-buffer t t)
-                 (goto-char curpnt))))))
+  (when yh-sql-do-format
+    (let* ((curbuf (current-buffer))
+           (curpnt (point))
+           (outbuf-name "*zetasql*")
+           (outbuf (get-buffer outbuf-name))
+           (fpath (buffer-file-name))
+           (command (yh-sql-format-command fpath)))
+      (when outbuf
+        (save-excursion
+          (switch-to-buffer outbuf)
+          (erase-buffer)))
+      ;; DIRTY HACK.
+      ;; zetasql-formatter return non-zero value when the formatted string is
+      ;; different from the original even if it was syntactically correct.
+      ;; Here, we want to change to *zetasql* buffern only when it is syntactically
+      ;; incorrect. We format the file twice and check the output of second run.
+      (let ((ret (progn (call-process-shell-command command
+                                                    nil outbuf-name)
+                        (call-process-shell-command command
+                                                    nil outbuf-name))))
+        (if (= ret 0)
+            (progn (switch-to-buffer curbuf)
+                   (revert-buffer t t)
+                   (goto-char curpnt)))))))
 
 (provide 'yh-sql)
 ;;; yh-sql.el ends here

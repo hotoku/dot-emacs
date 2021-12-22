d9e24a47ad8f825c1fb877168d5b8b371be9fe82
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2021-12-22 09:30:52 +0900

    zetasql

--- init.el
+++ init.el
@@ -88,13 +88,6 @@ The last executing date is recorded in the FILENAME in `user-emacs-directory.'"
 
 (use-package yh-sh :ensure nil)
 
-(use-package yh-sql :ensure nil
-  :hook
-  (sql-mode
-   .
-   (lambda ()
-     (add-hook 'after-save-hook 'yh-sql-format nil t))))
-
 (use-package yh-save :ensure nil)
 
 (use-package yh-pyenv :ensure nil)
@@ -559,7 +552,7 @@ https://github.com/ncaq/.emacs.d/blob/d1c8651f2683e110a6e4f7e3cd89c025812a6d0e/i
  '(custom-safe-themes
    '("57e3f215bef8784157991c4957965aa31bac935aca011b29d7d8e113a652b693" "246cd0eb818bfd347b20fb6365c228fddf24ab7164752afe5e6878cb29b0204e" default))
  '(package-selected-packages
-   '(helpful terraform-mode json-par git-modes session ppp spinner gnupg lsp-mode hcl-mode direx dired-k exec-path-from-shell dired yh-make il lsp-docker poetry gitignore-mode helm-ag pyenv afuternoon-theme afternoon-theme bazel-mode pyenv-mode-auto prettier-js dap-python py-autopep8 flymake-yaml dockerfile-mode biblio elpy haskell-mode yaml-mode json-mode gnu-elpa-keyring-update undo-tree git-ps1-mode ace-window flycheck yasnippet open-junk-file dakrone-theme smartparens helm company use-package)))
+   '(zetasql-formatter zetasql-formatter-mode helpful terraform-mode json-par git-modes session ppp spinner gnupg lsp-mode hcl-mode direx dired-k exec-path-from-shell dired yh-make il lsp-docker poetry gitignore-mode helm-ag pyenv afuternoon-theme afternoon-theme bazel-mode pyenv-mode-auto prettier-js dap-python py-autopep8 flymake-yaml dockerfile-mode biblio elpy haskell-mode yaml-mode json-mode gnu-elpa-keyring-update undo-tree git-ps1-mode ace-window flycheck yasnippet open-junk-file dakrone-theme smartparens helm company use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

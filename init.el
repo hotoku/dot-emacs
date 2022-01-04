27ec49a05dba6777a23eab6fe0aec46fc9b5dd7c
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-01-04 15:27:15 +0900

    menu-bar-mode off

--- init.el
+++ init.el
@@ -549,6 +549,7 @@ https://github.com/ncaq/.emacs.d/blob/d1c8651f2683e110a6e4f7e3cd89c025812a6d0e/i
 
 ;; aes
 (progn
+  (menu-bar-mode -1)
   (tool-bar-mode -1)
   (scroll-bar-mode -1)
   (column-number-mode)

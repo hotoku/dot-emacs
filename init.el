11d0cd32bfb6ec9c8f9276a8bc18e2ceb4b05ea5
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-01-05 18:10:37 +0900

    memo

--- init.el
+++ init.el
@@ -184,6 +184,7 @@ https://github.com/ncaq/.emacs.d/blob/d1c8651f2683e110a6e4f7e3cd89c025812a6d0e/i
   :config
   (bind-key "z" 'yh/dired-do-open dired-mode-map)
   (unless (equal (system-name) "JMB20200036.local")
+    ;; todo: この変数にhが入っていると、diredでディレクトリの色がおかしくなるときがある
     (setq dired-listing-switches "-alh")))
 
 (use-package savehist

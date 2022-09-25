e368d8ff7e5245b40f944999ccce498a354bc12b
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-09-25 11:08:14 +0900

    [markdown]

--- site-lisp/yh/yh-markdown.el
+++ site-lisp/yh/yh-markdown.el
@@ -11,9 +11,15 @@
   (interactive "srefnum: \nsurl: ")
   (save-excursion
     (goto-char (point-max))
+    (let ((link-keyword "<!-- link -->"))
+      (unless (search-backward link-keyword nil t)
+        (insert link-keyword "\n"))))
+
+  (save-excursion
+    (goto-char (point-max))
     (unless (= (char-before) 10)
       (insert "\n"))
-    (insert (format "[%s]: %s" refnum url))))
+    (insert (format "[%s]: %s\n" refnum url))))
 
 (provide 'yh-markdown)
 ;;; yh-markdown.el ends here

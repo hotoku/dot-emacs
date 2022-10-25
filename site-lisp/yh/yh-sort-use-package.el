14806d49b5f223c7f7a9774593ce22000bdfdbf6
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-10-26 05:53:46 +0900

    [yh-sup] yh-sup-make-stream does not work.

--- site-lisp/yh/yh-sort-use-package.el
+++ site-lisp/yh/yh-sort-use-package.el
@@ -10,7 +10,29 @@
 (defconst yh-sup-begin "yh-sup-begin")
 (defconst yh-sup-end "yh-sup-end")
 
-(defun yh-sup-extract-blocks (buf)
+(defun yh-sup-extract-block (buf)
+  "Extract use package block from BUF."
+  (save-window-excursion
+    (save-excursion
+      (let ((begin) (end))
+        (switch-to-buffer buf)
+        (goto-char (point-min))
+        (search-forward yh-sup-begin)
+        (end-of-line)
+        (setq begin (1+ (point)))
+        (search-forward yh-sup-end)
+        (beginning-of-line)
+        (setq end (1- (point)))
+        (buffer-substring-no-properties begin end)))))
+
+(defun yh-sup-make-stream (s)
+  "Make stream which provides contents of S."
+  (lambda (&optional unread)
+    (if unread (setq s (concat unread s))
+      (prog1 (substring s 0 1)
+        (setq s (substring s 1))))))
+
+(defun yh-sup-extract-sexps (buf)
   "Extract use package expressions from the buffer BUF."
   (let* ((curbuf (current-buffer))
          (obj)

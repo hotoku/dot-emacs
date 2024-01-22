326ed3e20560af2ad05f69dce98a7ce926e81da7
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2024-01-22 16:38:05 +0900

    color

--- init.el
+++ init.el
@@ -607,7 +607,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:background "AliceBlue" :foreground "DarkSlateGray4"))))
+ '(default ((t (:background "#D9CCC1" :foreground "DarkSlateGray4"))))
  )
 
 (provide 'init)

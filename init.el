84fd206986b5d8cb2a1a2ed359e3fda86fd94001
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2024-01-31 06:45:00 +0900

    blue is hard to see

--- init.el
+++ init.el
@@ -608,6 +608,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(font-lock-comment-face ((t (:foreground "color-219")))))
+ '(font-lock-comment-face ((t (:foreground "color-219"))))
+ '(font-lock-builtin-face ((t (:foreground "color-81")))))
 (provide 'init)
 ;;; init.el ends here
